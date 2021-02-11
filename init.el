ba6d4d8fcdaeac2251a7f663af1b3a22b407043e
Christian Ramet <git@christianramet.com>
2021-02-11 11:24:08 +0100

    eshell config indentation fix

--- init.el
+++ init.el
@@ -625,8 +625,8 @@ For ediff hooks usage"
     (company-mode -1)
     (setenv "PAGER""cat")
     (define-key eshell-mode-map (kbd "<tab>") 'completion-at-point)
-    (with-eval-after-load 'counsel)
-      (define-key eshell-mode-map (kbd "C-r") 'counsel-esh-history))
+    (with-eval-after-load 'counsel
+      (define-key eshell-mode-map (kbd "C-r") 'counsel-esh-history)))
 
   (add-hook 'eshell-mode-hook 'cr-eshell-settings))
 
