5092e0662a36dfd710b3ba25230fbf207d595a84
Christian Ramet <git@christianramet.com>
2021-03-23 11:19:09 +0100

    org blocks as lowercase (recommended) + snippet updates

--- init.el
+++ init.el
@@ -940,11 +940,17 @@ For ediff hooks usage"
                   (org-buffer-list 'files t))))
 
   (defun cr-org-pretty-symbols ()
+    (push '("#+begin_src"      . ?↦) prettify-symbols-alist)
     (push '("#+BEGIN_SRC"      . ?↦) prettify-symbols-alist)
+    (push '("#+end_src"        . ?⇤) prettify-symbols-alist)
     (push '("#+END_SRC"        . ?⇤) prettify-symbols-alist)
+    (push '("#+begin_quote"    . ?↦) prettify-symbols-alist)
     (push '("#+BEGIN_QUOTE"    . ?↦) prettify-symbols-alist)
+    (push '("#+end_quote"      . ?⇤) prettify-symbols-alist)
     (push '("#+END_QUOTE"      . ?⇤) prettify-symbols-alist)
+    (push '("#+begin_example"  . ?↦) prettify-symbols-alist)
     (push '("#+BEGIN_EXAMPLE"  . ?↦) prettify-symbols-alist)
+    (push '("#+end_example"    . ?⇤) prettify-symbols-alist)
     (push '("#+END_EXAMPLE"    . ?⇤) prettify-symbols-alist)
     (prettify-symbols-mode 1))
 
