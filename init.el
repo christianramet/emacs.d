ecbcfb4980e531cd3ca60803c885ed1a41c05c52
Christian Ramet <git@christianramet.com>
2021-03-23 22:14:33 +0100

    require ox-md after ox

--- init.el
+++ init.el
@@ -898,7 +898,7 @@ For ediff hooks usage"
   (org-use-tag-inheritance t)
   :config
   (require 'cr-private-org nil 'noerror)
-  (require 'ox-md nil 'noerror)
+  (with-eval-after-load 'ox (require 'ox-md nil 'noerror))
 
   (add-to-list 'org-speed-commands-user '("N" call-interactively 'org-metadown))
   (add-to-list 'org-speed-commands-user '("P" call-interactively 'org-metaup))
