e1834f2dec0e3160552b92008ddccd422ba02707
Christian Ramet <git@christianramet.com>
2021-03-27 22:38:29 +0100

    french-holidays.el documentation

--- lisp/french-holidays.el
+++ lisp/french-holidays.el
@@ -1,8 +1,39 @@
-; Source: https://www.emacswiki.org/emacs/french-holidays.el
-; To show French holidays (and only those) in your Emacs calendar,
-; put the following lines into your .emacs:
-;   (require 'french-holidays)
-;   (setq calendar-holidays holiday-french-holidays)
+;;; french-holidays.el --- French holidays -*- lexical-binding: t -*-
+
+;; Author: Christian Ramet
+;; Maintainer: Christian Ramet
+;; Version: 1.0
+;; Package-Requires:
+;; Homepage:
+;; Keywords:
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; To show French holidays (and only those) in your Emacs calendar,
+;; put the following lines into your .emacs:
+;;   (require 'french-holidays)
+;;   (setq calendar-holidays holiday-french-holidays)
+
+;; Source: https://www.emacswiki.org/emacs/french-holidays.el
+
+;;; Code:
 
 (eval-when-compile
   (require 'calendar)
@@ -34,3 +65,5 @@
         (holiday-float 6 0 3 "Fête des pères")))
 
 (provide 'french-holidays)
+
+;;; french-holidays.el ends here
