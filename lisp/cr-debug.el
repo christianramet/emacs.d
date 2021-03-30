530684ae2c1c598e56e52d85b41f144775c838cd
Christian Ramet <git@christianramet.com>
2021-03-30 19:49:53 +0200

    verbose debuging

--- lisp/cr-debug.el
+++ lisp/cr-debug.el
@@ -37,6 +37,13 @@
  '(use-package-expand-minimally nil)
  '(use-package-compute-statistics t))
 
+(custom-set-variables
+ '(auto-revert-verbose t)
+ '(byte-compile-verbose t)
+ '(magit-refresh-verbose t)
+ '(projectile-verbose t)
+ '(which-key-is-verbose t))
+
 (with-eval-after-load 'use-package
   (add-hook 'after-init-hook 'use-package-report))
 
