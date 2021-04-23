080fa244afd5a7d66be0492e26833c7dc98e2d8d
Christian Ramet <git@christianramet.com>
2021-04-23 11:23:29 +0200

    removed lib headers

--- lisp/doom-sudo-utils.el
+++ lisp/doom-sudo-utils.el
@@ -1,34 +1,4 @@
-;;; doom-sudo-utils.el --- sudo utilities -*- lexical-binding: t -*-
-
-;; Author: Christian Ramet
-;; Maintainer: Christian Ramet
-;; Version: 1.0
-;; Package-Requires:
-;; Homepage:
-;; Keywords: sudo
-
-
-;; This file is not part of GNU Emacs
-
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
-
-
-;;; Commentary:
-
-;; Sudo utilities, picked from Doom Emacs 'files.el' library.
-
-;;; Code:
+;; sudo utilities, picked from Doom Emacs 'files.el' library.
 
 (defun doom--sudo-file-path (file)
   (let ((host (or (file-remote-p file 'host) "localhost")))
@@ -71,5 +41,3 @@
           default-directory)))))
 
 (provide 'doom-sudo-utils)
-
-;;; doom-sudo-utils.el ends here
