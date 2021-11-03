683ece41f04487c4a50b9868eef1f143c013699c
Christian Ramet <git@christianramet.com>
2021-11-03 23:05:23 +0100

    flymake-show-buffer-diagnostics (was using an alias)

--- init.el
+++ init.el
@@ -671,7 +671,7 @@ remain in fixed pitch for the tags to be aligned."
 (use-package flymake
   :straight (:type built-in)
   :hook (prog-mode . flymake-mode)
-  :bind (("C-c !" . flymake-show-diagnostics-buffer)
+  :bind (("C-c !" . flymake-show-buffer-diagnostics)
          :map cr-toggle-map ("f" . flymake-mode)))
 
 (use-package flyspell
