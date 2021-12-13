640b70924d9b7635ee711554aeb87ad264115e43
test <git@christianramet.com>
2021-12-13 20:28:10 +0100

    command as meta on macos

--- init.el
+++ init.el
@@ -126,11 +126,19 @@
  '(mouse-wheel-progressive-speed t)
  '(next-screen-context-lines 5))
 
+;; (when system-is-osx-p
+;;   (custom-set-variables
+;;    '(mac-option-modifier 'meta)
+;;    '(mac-command-modifier 'none)
+;;    '(mac-right-option-modifier 'none)))
+
 (when system-is-osx-p
   (custom-set-variables
-   '(mac-option-modifier 'meta)
-   '(mac-command-modifier 'none)
-   '(mac-right-option-modifier 'none)))
+   '(mac-pass-command-to-system nil)
+   '(mac-pass-control-to-system t)
+   '(mac-command-modifier 'meta)
+   '(mac-option-modifier '(:function alt :mouse alt))
+   '(mac-right-option-modifier 'left)))
 
 ;;; Personal prefix maps key bindings
 (define-prefix-command 'cr-app-map)
