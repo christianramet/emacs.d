b5a94b58ed76b6914192f1324968a7aaf271ff8c
Christian Ramet <git@christianramet.com>
2021-12-14 10:11:36 +0100

    org startup folded

--- init.el
+++ init.el
@@ -870,7 +870,7 @@ remain in fixed pitch for the tags to be aligned."
   (org-src-preserve-indentation t)
   (org-src-tab-acts-natively t)
   (org-src-window-setup 'current-window)
-  (org-startup-folded nil)
+  (org-startup-folded t)
   (org-startup-with-inline-images t)
   (org-use-speed-commands t)
   :config
