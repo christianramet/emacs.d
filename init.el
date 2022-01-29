6d93c61d6fed3080fccf00b8ee5116a247dc1dda
Christian Ramet <git@christianramet.com>
2022-01-29 23:57:47 +0100

    MacOS font size for var pitch

--- init.el
+++ init.el
@@ -590,7 +590,7 @@ Documentation: https://github.com/ytdl-org/youtube-dl#format-selection"
   (when system-is-osx-p
     (set-face-attribute 'default nil :font "Monaco-13")
     (set-face-attribute 'fixed-pitch nil :font "Monaco-13")
-    (set-face-attribute 'variable-pitch nil :font "Helvetica-15")))
+    (set-face-attribute 'variable-pitch nil :font "Helvetica-16")))
 
 (use-package face-remap
   :straight (:type built-in)
